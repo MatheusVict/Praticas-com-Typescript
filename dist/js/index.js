4967880088ad56bdcfa60af063c9a3190900a764
MatheusVict <matheusvictorhenrique@gmail.com>
2022-08-08 17:26:56 -0300

    continuação do aprendizado

--- dist/js/index.js
+++ dist/js/index.js
@@ -16,7 +16,31 @@ console.log(typeof idade);
 console.log(typeof adm);
 // object
 const numero = [5, 6, 7]; // colocar o tipo e um [] define q é um array númerico
-console.log(numero);
+//console.log(numero);
 console.log(numero.length);
-//console.log(numero.toUpperCase()) não funciona pq é um array numerico
+// console.log(numero.toUpperCase()) //não funciona pq é um array numerico
 console.log(nome.toUpperCase());
+numero.push(2);
+console.log(numero);
+// Tuplas
+let mTuple; // Determinadno o tipo da tupla e tem q seguir exatamente essa ordem
+mTuple = ['oi', 5, ['oi']];
+// object literals -> {prop: value}
+const user = {
+    name: 'Matheus',
+    age: 8
+};
+console.log(user);
+console.log(user.name);
+// user.job = 'Dev' //Não funciona pq esse atributo não existe no array
+// any = valor q aceita qualquer tipo de dado
+let m = 0;
+m = 'vai';
+m = true;
+m = [];
+// union type
+// Divide os tipos por uma pipe, quer dizer q vai receber diferentes tipos
+let id = 10;
+id = 'olá';
+const userId = 10; // Todos compartilham de um tipo, oq facilita muito
+const productId = 'pc';
